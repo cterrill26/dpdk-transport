872c9e1c719db4895e3da1224a3973894af4e6d8
cterrill26 <cdterrill26@gmail.com>
2023-02-28 13:49:23 -0800

    Edit msginfo

--- dpdk_transport.h
+++ dpdk_transport.h
@@ -3,18 +3,19 @@
 
 #define MAX_MSG_SiZE 100000
 
-struct addrinfo
+struct msginfo
 {
 	uint32_t src_ip;
 	uint32_t dst_ip;
 	uint64_t src_mac;
 	uint64_t dst_mac;
+	uint32_t length;
+	uint16_t portid;
 };
 
-void run(uint16_t portid);
-void terminate(uint16_t portid);
-void send(uint16_t portid, const void *buffer, uint32_t length, const struct addrinfo *info);
-uint32_t read(uint16_t portid, void *buffer, struct addrinfo *info);
+int init(int argc, char *argv[]);
+int sendmsg(const void *buffer, const struct msginfo *info);
+uint32_t recvmsg(void *buffer, struct msginfo *info);
 uint64_t port_to_mac(uint16_t portid);
 uint32_t string_to_ip(char *s);
 uint64_t string_to_mac(char *s);
