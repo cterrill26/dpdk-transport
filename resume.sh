2b8f6308dc2f0690ddd82e0bd5743e99a07406cf
root <root@ip-172-31-10-165.us-west-2.compute.internal>
2023-04-06 23:14:23 +0000

    Redo make structure with new directories

---
+++ resume.sh
@@ -0,0 +1,4 @@
+cd /home/ec2-user/dpdk-stable-19.11.14
+export RTE_SDK=`pwd`
+export RTE_TARGET=x86_64-native-linuxapp-gcc
+cd $OLDPWD
---
+++ setup.sh
@@ -0,0 +1,13 @@
+cd /home/ec2-user/dpdk-stable-19.11.14
+export RTE_SDK=`pwd`
+export RTE_TARGET=x86_64-native-linuxapp-gcc
+echo 1024 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+echo 0 > /proc/sys/kernel/randomize_va_space
+modprobe uio
+modprobe hwmon
+insmod ./x86_64-native-linuxapp-gcc/kmod/rte_kni.ko
+insmod ./x86_64-native-linuxapp-gcc/kmod/igb_uio.ko
+export DPDK_IPV4=$(ifconfig eth1 | grep 'inet ' | awk '{print $2}') 
+ifconfig eth1 down
+./usertools/dpdk-devbind.py --bind=igb_uio eth1
+cd $OLDPWD
