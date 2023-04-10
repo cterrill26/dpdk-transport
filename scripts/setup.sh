cb42fedc443ecadfa5746ff98aa3a211c24cbb5f
root <root@ip-172-31-10-165.us-west-2.compute.internal>
2023-04-10 06:48:47 +0000

    Edit scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,14 +1,6 @@
-cd /home/ec2-user/dpdk-stable-19.11.14
-export RTE_SDK=`pwd`
+export RTE_SDK=/home/ec2-user/dpdk-stable-19.11.14
 export RTE_TARGET=x86_64-native-linuxapp-gcc
-echo 1024 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
-echo 0 > /proc/sys/kernel/randomize_va_space
-modprobe uio
-modprobe hwmon
-insmod ./x86_64-native-linuxapp-gcc/kmod/rte_kni.ko
-insmod ./x86_64-native-linuxapp-gcc/kmod/igb_uio.ko
 export DPDK_IPV4=$(ifconfig eth1 | grep 'inet ' | awk '{print $2}') 
 ifconfig eth1 down
-./usertools/dpdk-devbind.py --bind=igb_uio eth1
+$RTE_SDK/usertools/dpdk-devbind.py --bind=igb_uio eth1
 sleep 1
-cd $OLDPWD
