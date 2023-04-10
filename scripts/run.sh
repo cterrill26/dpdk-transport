0e3a5446a79af60736ccebf1b18d7290017ab0d5
root <root@ip-172-31-10-165.us-west-2.compute.internal>
2023-04-10 07:10:43 +0000

    Fix path

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 source scripts/setup.sh
 make
-.tests/initiator/$RTE_TARGET/app/initiator -- -s $DPDK_IPV4 -d 172.31.6.161 -m 0a:0a:fb:9e:c1:f1 
+./tests/initiator/$RTE_TARGET/app/initiator -- -s $DPDK_IPV4 -d 172.31.6.161 -m 0a:0a:fb:9e:c1:f1 
